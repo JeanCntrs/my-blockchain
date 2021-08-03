371542cc5345d8d38f7ede79a2b893b1de329910
Jean Contreras <jcontreras@stratechcorp.com>
2021-08-02 22:52:42 -0400

    Added generation of the genesis block

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import PKG from "./package.json";
-import Block from "./src/class/Block";
+import PKG from './package.json';
+import Block from './src/class/Block';
 
 const { name, version } = PKG;
 
 console.log(`${name} - ${version}`);
 
-const block = new Block(Date.now(), "previous_hash", "hash", "data");
+const { genesis } = Block;
+console.log(genesis.toString());
+
+const block = new Block(Date.now(), genesis.hash, 'hash', 'data');
 console.log(block.toString());
+
+const block2 = new Block(Date.now(), block.hash, 'h4sh', 'new data');
+console.log(block2.toString());
--- src/class/Block.js
+++ src/class/Block.js
@@ -6,6 +6,11 @@ class Block {
     this.data = data;
   }
 
+  static get genesis() {
+    const timestamp = (new Date(2000, 0, 1)).getTime();
+    return new this(timestamp, undefined, 'g3n3s1s_h4sh', 'I Like Ramen.');
+  }
+
   toString() {
     const { timestamp, previousHash, hash, data } = this;
 
