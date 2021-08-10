50a9e5e162eee8dd587bc8e36ce53d6b1ef34e32
Jean C. Contreras <jean.cntrs@gmail.com>
2021-08-10 18:27:42 -0400

    Added and configure express

---
+++ src/service/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import Blockchain from '../classes/Blockchain';
+
+const { HTTP_PORT = 3000 } = process.env;
+const app = express();
+const blockchain = new Blockchain();
+
+app.use(bodyParser.json());
+
+app.listen(HTTP_PORT, () => {
+    console.log(`Service is running on port:${HTTP_PORT}`);
+});
