6b6eeb3eaf76d6d01ed8c7723f3eedd5cd6c4772
Jean C. Contreras <jean.cntrs@gmail.com>
2021-08-11 21:11:01 -0400

    Added endpoint to get blocks

--- src/service/index.js
+++ src/service/index.js
@@ -8,6 +8,10 @@ const blockchain = new Blockchain();
 
 app.use(bodyParser.json());
 
+app.get('/blocks', (req, res) => {
+    res.json(blockchain.blocks);
+});
+
 app.listen(HTTP_PORT, () => {
     console.log(`Service is running on port:${HTTP_PORT}`);
 });
