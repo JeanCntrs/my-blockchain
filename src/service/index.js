ed7fbb43d469689353da6418697b3b291ba5d609
Jean C. Contreras <jean.cntrs@gmail.com>
2021-08-11 21:37:29 -0400

    Added endpoint to mine new blocks

--- src/service/index.js
+++ src/service/index.js
@@ -12,6 +12,13 @@ app.get('/blocks', (req, res) => {
     res.json(blockchain.blocks);
 });
 
+app.post('/mine', (req, res) => {
+    const { body: { data } } = req;
+    const block = blockchain.addBlock(data);
+
+    res.json({ blocks: blockchain.blocks.length, block });
+});
+
 app.listen(HTTP_PORT, () => {
     console.log(`Service is running on port:${HTTP_PORT}`);
 });
