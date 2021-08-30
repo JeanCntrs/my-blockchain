b0fbe18ac64af028b22e93e210aac0d2872c8de7
Jean C. Contreras <jean.cntrs@gmail.com>
2021-08-30 19:08:23 -0400

    Was added more functionality the function to mine

--- src/classes/Miner.js
+++ src/classes/Miner.js
@@ -1,3 +1,8 @@
+import Transaction from "./Transaction";
+import Wallet from "./Wallet";
+
+const blockchainWallet = new Wallet();
+
 class Miner {
     constructor(blockchain, p2pservice, wallet) {
         this.blockchain = blockchain;
@@ -6,7 +11,17 @@ class Miner {
     }
 
     mine() {
-        const { blockchain: { memoryPool } } = this;
+        const { blockchain: { memoryPool }, p2pservice, wallet } = this;
+
+        if (memoryPool.transactions.length === 0) throw Error('There are no unconfirmed transactions');
+
+        memoryPool.transactions.push(Transaction.reward(this.wallet, blockchainWallet));
+
+        const block = this.blockchain.addBlock(memoryPool.transactions);
+
+        p2pservice.sync();
+
+        return block
     }
 }
 
