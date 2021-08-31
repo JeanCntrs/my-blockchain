bfccc83bd28536663ef14e14547f241ec07bfcbb
Jean C. Contreras <jean.cntrs@gmail.com>
2021-08-31 18:40:22 -0400

    Was added endpoint to mine transactions

--- src/service/index.js
+++ src/service/index.js
@@ -3,12 +3,15 @@ import bodyParser from 'body-parser';
 import Blockchain from '../classes/Blockchain';
 import Wallet from '../classes/Wallet';
 import P2PService, { MESSAGE } from './p2p';
+import Miner from '../classes/Miner';
 
 const { HTTP_PORT = 3000 } = process.env;
 const app = express();
 const blockchain = new Blockchain();
 const wallet = new Wallet(blockchain);
+const walletMiner = new Wallet(blockchain, 0);
 const p2pService = new P2PService(blockchain);
+const miner = new Miner(blockchain, p2pService, walletMiner);
 
 app.use(bodyParser.json());
 
@@ -41,6 +44,15 @@ app.post('/transaction', (req, res) => {
         res.json(txn);
     } catch (error) {
         console.log('error', error);
+        res.json({ error: error.message });
+    }
+});
+
+app.get('/mine/transactions', (req, res) => {
+    try {
+        miner.mine();
+        res.redirect('/blocks');
+    } catch (error) {
         res.json({ error: error.message });
     }
 });
